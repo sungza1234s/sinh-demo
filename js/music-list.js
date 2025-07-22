d0585a2a8dd3cefbadaca2ae1d6ee04614fe0bee
sungza1234s <sungza123s@gmail.com>
2025-07-23 00:59:38 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,63 @@
+let allMusic = [
+  {
+    name: "AI LÀ NGƯỜI THƯƠNG EM (TWAN x QTRUNG REMIX)",
+    artist: "Quân A.P",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Anh Đã Từng Cố Gắng (Hieii Remix)",
+    artist: "Nhật Phong",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Cảm Ơn Vì Tất Cả (RIN Music Remix)",
+    artist: "Anh Quân Idol",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Chẳng Cần Ngày Mai (Remix)",
+    artist: "Trung I.U",
+    img: "music-4",
+    src: "music-4"
+  },
+    {
+    name: "Cơ Hội Cuối (Style Huy PT Remix)",
+    artist: "An Vũ",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Nắng Dưới Chân Mây (Style Huy PT Remix)",
+    artist: "Nguyễn Hữu Kha",
+    img: "music-6",
+    src: "music-6"
+  },
+    {
+    name: "TAY NĂM TAY RƠI (ft. TranVinh Remix)",
+    artist: "VwZuong",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Thay Tôi Yêu Cô Ấy (Style Huy PT Remix)",
+    artist: "Thanh Hưng",
+    img: "music-8",
+    src: "music-8"
+  },
+      {
+    name: "Cố Quên Đi Một Người (TB Remix)",
+    artist: " Vũ Quốc Nhật ",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Em Cưới Rồi À Remix (Đông x Thazh)",
+    artist: "Thanh Hưng x H2O ",
+    img: "music-10",
+    src: "music-10"
+  },
+
+];
